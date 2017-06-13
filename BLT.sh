ecefed8d18fe606efceb498e578b7d9463a7b1f5
Brian Sutjiadi <bsutjiad@ucsd.edu>
2017-06-12 23:25:53 -0700

    Polished the alignment and variant calling portion of the script

--- BLT.sh
+++ BLT.sh
@@ -1,85 +1,113 @@
 #!/bin/bash
 
-# 1 --> reference
-# 2 --> 
-
-# Set working directory
-DIR=/home/bimm185
-
 # Input argument parsing
 if [ $1 = "-h" ]
 then
-    echo "USAGE: BLT reference sample1forward sample1reverse sample2forward sample2reverse"
+    echo "USAGE: BLT reference.fasta data_dir sample1root sample2root"
 
     exit 1
 fi
 
-if [ $# != 5 ] 
+if [ $# != 4 ] 
 then
     echo "wrong number of files"
-    echo "USAGE: BLT reference sample1forward sample1reverse sample2forward sample2reverse"
+    echo "USAGE: BLT reference.fasta data_dir sample1root sample2root"
 
     exit 1
 fi
 
-mkdir $DIR/working_test
-mkdir $DIR/working_test/fastqc
+# Set working directory and other variables
+HOMEDIR=/home/`whoami`
+REFERENCE=$1
+DATADIR=$2
+SAMPLE1=$3
+SAMPLE2=$4
+HOMEDIR=${HOMEDIR%%\/}
+DATADIR=${DATADIR%%\/}
 
-# for f in $(ls $DIR/data/*.fastq); do echo $f; fastqc -o $DIR/working_test/fastqc $f ; done
+mkdir $HOMEDIR/working
+mkdir $HOMEDIR/working/fastqc
 
-for f in $(ls $DIR/data/*.fastq); do echo $f; python $DIR/BIMM185FinalProject/modfastq.py $f ${f/data/working_test}; done
-
-
-
-#bwa index -p $f{${f/reference/working_test}%%.fna}; done
-cp $DIR/reference/GCF_000006945.2_ASM694v2_genomic.fna $DIR/working_test/.
-for f in $(ls $DIR/working_test/*.fna); do echo $f; bwa index $f; done
-
-# UC8
-bwa mem $DIR/working_test/GCF_000006945.2_ASM694v2_genomic.fna $DIR/working_test/UC8_1.fastq $DIR/working_test/UC8_2.fastq > $DIR/working_test/UC8.sam
-bwa mem $DIR/working_test/GCF_000006945.2_ASM694v2_genomic.fna $DIR/working_test/UC9_1.fastq $DIR/working_test/UC9_2.fastq > $DIR/working_test/UC9.sam
-
-samtools view -o $DIR/working_test/UC8.bam -b $DIR/working_test/UC8.sam
-samtools view -o $DIR/working_test/UC9.bam -b $DIR/working_test/UC9.sam
-
-samtools flagstat $DIR/working_test/UC8.bam > $DIR/working_test/UC8_align_stats.txt
-samtools flagstat $DIR/working_test/UC9.bam > $DIR/working_test/UC9_align_stats.txt
-
-samtools sort $DIR/working_test/UC8.bam -o $DIR/working_test/UC8_sorted.bam 
-samtools sort $DIR/working_test/UC9.bam -o $DIR/working_test/UC9_sorted.bam 
-
-samtools mpileup -f $DIR/working_test/GCF_000006945.2_ASM694v2_genomic.fna $DIR/working_test/UC8_sorted.bam > $DIR/working_test/UC8.mpileup
-samtools mpileup -f $DIR/working_test/GCF_000006945.2_ASM694v2_genomic.fna $DIR/working_test/UC9_sorted.bam > $DIR/working_test/UC9.mpileup
-
-java -Xmx4g -jar ~/VarScan.v2.4.3.jar mpileup2snp $DIR/working_test/UC8.mpileup --min-var-freq 0.7 --variants --output-vcf 1 > $DIR/working_test/UC8.vcf
-java -Xmx4g -jar ~/VarScan.v2.4.3.jar mpileup2snp $DIR/working_test/UC9.mpileup --min-var-freq 0.7 --variants --output-vcf 1 > $DIR/working_test/UC9.vcf
+echo "===========Starting FastQC==========="
+for f in $(ls $DATADIR/*.fastq); do echo "Performing fastqc on $f..."; fastqc -o $HOMEDIR/working/fastqc $f ; done
+echo "===========Done with FastQC==========="
 
 
+echo "===========Performing Alignment==========="
+# Copy all required files to the working directory
+cp $REFERENCE $HOMEDIR/working/
+cp $DATADIR/*.fastq $HOMEDIR/working/
+REFERENCE=${REFERENCE##*/}
 
-#VCF File comparison
-#get header                                                                     
+pushd $HOMEDIR/working/
 
-grep '#' $DIR/working/UC8.vcf > $DIR/working_test/VCF_header.txt
-header=$(grep -c '#' $DIR/working/UC8.vcf)
-header="$((header+1))"
+# Index the reference
+bwa index $REFERENCE
 
-echo 'number of headers'
-echo $header
-tail -n +$header $DIR/working_test/UC8.vcf | awk '{printf("%s:%s:%s %s %s %s %s %s %s %s %s %s %s\n", $2, $4, $5, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10);}' | sort > $DIR/working_test/UC8_sorted_vcf
-tail -n +$header $DIR/working_test/UC9.vcf | awk '{printf("%s:%s:%s %s %s %s %s %s %s %s %s %s %s\n", $2, $4, $5, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10);}' | sort > $DIR/working_test/UC9_sorted_vcf
+echo "Align using bwa mem..."
+bwa mem $REFERENCE ${SAMPLE1}_1.fastq ${SAMPLE1}_2.fastq > ${SAMPLE1}.sam
+bwa mem $REFERENCE ${SAMPLE2}_1.fastq ${SAMPLE2}_2.fastq > ${SAMPLE2}.sam
 
-mkdir $DIR/working_test/vcf_output
+echo "Convert to bam..."
+samtools view -o ${SAMPLE1}.bam -b ${SAMPLE1}.sam
+samtools view -o ${SAMPLE2}.bam -b ${SAMPLE2}.sam
 
-join -1 1 -2 1 -o 1.3,1.5,1.6 $DIR/working_test/UC8_sorted_vcf $DIR/working_test/UC9_sorted_vcf > $DIR/working_test/vcf_output/common.vcf
+echo "Get alignment stats..."
+samtools flagstat ${SAMPLE1}.bam > ${SAMPLE1}_align_stats.txt
+samtools flagstat ${SAMPLE2}.bam > ${SAMPLE2}_align_stats.txt
 
-join -1 1 -2 1 -v1 $DIR/working_test/UC8_sorted_vcf $DIR/working_test/UC9_sorted_vcf > $DIR/working_test/vcf_output/only_UC8.vcf
+echo "Sort the alignment..."
+samtools sort ${SAMPLE1}.bam -o ${SAMPLE1}_sorted.bam 
+samtools sort ${SAMPLE2}.bam -o ${SAMPLE2}_sorted.bam 
 
-awk '{$(NF+1)=$1;$1=""}sub(FS,"")' $DIR/working_test/vcf_output/only_UC8.vcf > $DIR/working_test/vcf_output/only_UC8_orig.vcf
+popd
 
-cat $DIR/working_test/VCF_header.txt $DIR/working_test/vcf_output/only_UC8_orig.vcf > $DIR/working_test/vcf_output/only_UC8.vcf
+echo "===========Done with Alignment==========="
 
-join -1 1 -2 1 -v2 $DIR/working_test/UC8_sorted_vcf $DIR/working_test/UC9_sorted_vcf > $DIR/working_test/vcf_output/only_UC9.vcf
+echo "===========Creating mpileup==========="
+pushd $HOMEDIR/working/
 
-awk '{$(NF+1)=$1;$1=""}sub(FS,"")' $DIR/working_test/vcf_output/only_UC9.vcf > $DIR/working_test/vcf_output/only_UC9_orig.vcf
+samtools mpileup -f $REFERENCE ${SAMPLE1}_sorted.bam > ${SAMPLE1}.mpileup
+samtools mpileup -f $REFERENCE ${SAMPLE2}_sorted.bam > ${SAMPLE2}.mpileup
 
-cat $DIR/working_test/VCF_header.txt $DIR/working_test/vcf_output/only_UC9_orig.vcf > $DIR/working_test/vcf_output/only_UC9.vcf
+popd
+echo "===========Done with mpileup==========="
+
+echo "===========Calling Variants==========="
+pushd $HOMEDIR/working/
+
+java -Xmx4g -jar ~/VarScan.v2.4.3.jar mpileup2snp ${SAMPLE1}.mpileup --min-var-freq 0.7 --variants --output-vcf 1 > ${SAMPLE1}.vcf
+java -Xmx4g -jar ~/VarScan.v2.4.3.jar mpileup2snp ${SAMPLE2}.mpileup --min-var-freq 0.7 --variants --output-vcf 1 > ${SAMPLE2}.vcf
+
+popd
+echo "===========Done with Variant Calling==========="
+
+
+
+# #VCF File comparison
+# #get header                                                                     
+# 
+# grep '#' $DIR/working/UC8.vcf > $DIR/working_test/VCF_header.txt
+# header=$(grep -c '#' $DIR/working/UC8.vcf)
+# header="$((header+1))"
+# 
+# echo 'number of headers'
+# echo $header
+# tail -n +$header $DIR/working_test/UC8.vcf | awk '{printf("%s:%s:%s %s %s %s %s %s %s %s %s %s %s\n", $2, $4, $5, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10);}' | sort > $DIR/working_test/UC8_sorted_vcf
+# tail -n +$header $DIR/working_test/UC9.vcf | awk '{printf("%s:%s:%s %s %s %s %s %s %s %s %s %s %s\n", $2, $4, $5, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10);}' | sort > $DIR/working_test/UC9_sorted_vcf
+# 
+# mkdir $DIR/working_test/vcf_output
+# 
+# join -1 1 -2 1 -o 1.3,1.5,1.6 $DIR/working_test/UC8_sorted_vcf $DIR/working_test/UC9_sorted_vcf > $DIR/working_test/vcf_output/common.vcf
+# 
+# join -1 1 -2 1 -v1 $DIR/working_test/UC8_sorted_vcf $DIR/working_test/UC9_sorted_vcf > $DIR/working_test/vcf_output/only_UC8.vcf
+# 
+# awk '{$(NF+1)=$1;$1=""}sub(FS,"")' $DIR/working_test/vcf_output/only_UC8.vcf > $DIR/working_test/vcf_output/only_UC8_orig.vcf
+# 
+# cat $DIR/working_test/VCF_header.txt $DIR/working_test/vcf_output/only_UC8_orig.vcf > $DIR/working_test/vcf_output/only_UC8.vcf
+# 
+# join -1 1 -2 1 -v2 $DIR/working_test/UC8_sorted_vcf $DIR/working_test/UC9_sorted_vcf > $DIR/working_test/vcf_output/only_UC9.vcf
+# 
+# awk '{$(NF+1)=$1;$1=""}sub(FS,"")' $DIR/working_test/vcf_output/only_UC9.vcf > $DIR/working_test/vcf_output/only_UC9_orig.vcf
+# 
+# cat $DIR/working_test/VCF_header.txt $DIR/working_test/vcf_output/only_UC9_orig.vcf > $DIR/working_test/vcf_output/only_UC9.vcf
