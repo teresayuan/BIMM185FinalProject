ec14b70578eba43999bd3bd7a312bbb4c0278954
Brian Sutjiadi <bsutjiad@ucsd.edu>
2017-06-13 00:57:11 -0700

    Integrated SNP processing into the pipeline

--- BLT.sh
+++ BLT.sh
@@ -84,30 +84,31 @@ echo "===========Done with Variant Calling==========="
 
 
 
-# #VCF File comparison
-# #get header                                                                     
-# 
-# grep '#' $DIR/working/UC8.vcf > $DIR/working_test/VCF_header.txt
-# header=$(grep -c '#' $DIR/working/UC8.vcf)
-# header="$((header+1))"
-# 
-# echo 'number of headers'
-# echo $header
-# tail -n +$header $DIR/working_test/UC8.vcf | awk '{printf("%s:%s:%s %s %s %s %s %s %s %s %s %s %s\n", $2, $4, $5, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10);}' | sort > $DIR/working_test/UC8_sorted_vcf
-# tail -n +$header $DIR/working_test/UC9.vcf | awk '{printf("%s:%s:%s %s %s %s %s %s %s %s %s %s %s\n", $2, $4, $5, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10);}' | sort > $DIR/working_test/UC9_sorted_vcf
-# 
-# mkdir $DIR/working_test/vcf_output
-# 
-# join -1 1 -2 1 -o 1.3,1.5,1.6 $DIR/working_test/UC8_sorted_vcf $DIR/working_test/UC9_sorted_vcf > $DIR/working_test/vcf_output/common.vcf
-# 
-# join -1 1 -2 1 -v1 $DIR/working_test/UC8_sorted_vcf $DIR/working_test/UC9_sorted_vcf > $DIR/working_test/vcf_output/only_UC8.vcf
-# 
-# awk '{$(NF+1)=$1;$1=""}sub(FS,"")' $DIR/working_test/vcf_output/only_UC8.vcf > $DIR/working_test/vcf_output/only_UC8_orig.vcf
-# 
-# cat $DIR/working_test/VCF_header.txt $DIR/working_test/vcf_output/only_UC8_orig.vcf > $DIR/working_test/vcf_output/only_UC8.vcf
-# 
-# join -1 1 -2 1 -v2 $DIR/working_test/UC8_sorted_vcf $DIR/working_test/UC9_sorted_vcf > $DIR/working_test/vcf_output/only_UC9.vcf
-# 
-# awk '{$(NF+1)=$1;$1=""}sub(FS,"")' $DIR/working_test/vcf_output/only_UC9.vcf > $DIR/working_test/vcf_output/only_UC9_orig.vcf
-# 
-# cat $DIR/working_test/VCF_header.txt $DIR/working_test/vcf_output/only_UC9_orig.vcf > $DIR/working_test/vcf_output/only_UC9.vcf
+echo "===========Analyzing SNPs==========="
+#VCF File comparison
+#get header                                                                     
+
+pushd $HOMEDIR/working/
+mkdir tmp
+mkdir vcf_output
+
+# Assume VCF headers are similar, so save an arbitrary one
+grep '#' ${SAMPLE1}.vcf > tmp/VCF_header.txt
+header=$(grep -c '#' ${SAMPLE1}.vcf)
+header="$((header+1))"
+
+tail -n +$header ${SAMPLE1}.vcf | awk '{printf("%s:%s:%s %s %s %s %s %s %s %s %s %s %s\n", $2, $4, $5, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10);}' | sort > ${SAMPLE1}_sorted_vcf
+tail -n +$header ${SAMPLE2}.vcf | awk '{printf("%s:%s:%s %s %s %s %s %s %s %s %s %s %s\n", $2, $4, $5, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10);}' | sort > ${SAMPLE2}_sorted_vcf
+
+join -1 1 -2 1 -o 1.3,1.5,1.6 ${SAMPLE1}_sorted_vcf ${SAMPLE2}_sorted_vcf > vcf_output/common.vcf
+join -1 1 -2 1 -v1 ${SAMPLE1}_sorted_vcf ${SAMPLE2}_sorted_vcf > vcf_output/only_${SAMPLE1}.vcf
+awk '{$(NF+1)=$1;$1=""}sub(FS,"")' vcf_output/only_${SAMPLE1}.vcf > tmp/only_${SAMPLE1}_orig.vcf
+cat tmp/VCF_header.txt tmp/only_${SAMPLE1}_orig.vcf > vcf_output/only_${SAMPLE1}.vcf
+join -1 1 -2 1 -v2 ${SAMPLE1}_sorted_vcf ${SAMPLE2}_sorted_vcf > vcf_output/only_${SAMPLE2}.vcf
+awk '{$(NF+1)=$1;$1=""}sub(FS,"")' vcf_output/only_${SAMPLE2}.vcf > tmp/only_${SAMPLE2}_orig.vcf
+cat tmp/VCF_header.txt tmp/only_${SAMPLE2}_orig.vcf > vcf_output/only_${SAMPLE2}.vcf
+
+rm -rf tmp
+
+popd
+echo "===========Done analyzing SNPs==========="
