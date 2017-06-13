17e7c20c93589f0270897bf88653c3bd5caa30e9
Brian Sutjiadi <bsutjiad@ucsd.edu>
2017-06-13 01:07:48 -0700

    Replacing BLT.sh with 185.sh

--- 185.sh
+++ 185.sh
@@ -1,91 +1,116 @@
 #!/bin/bash
 
-# 1 --> reference
-# 2 --> forward strand of sample 1
-# 3 --> reverse strand of sample 1
-# 4 --> forward strand of sample 2
-# 5 --> reverse strand of sample 2
-
-
-if [ "$1" == "-h" ] || [ "$#" -ne 5 ];
+# Input argument parsing
+if [ $1 = "-h" ]
 then
-	if [ "$1" != "-h" ];
-	then
-		echo "wrong number of files"
-	fi
-	echo "BLT.sh reference sample1forward sample1reverse sample2forward sample2reverse"
+    echo "USAGE: BLT reference.fasta data_dir sample1root sample2root"
 
-	exit 1
+    exit 1
 fi
+
+if [ $# != 4 ] 
+then
+    echo "wrong number of files"
+    echo "USAGE: BLT reference.fasta data_dir sample1root sample2root"
+
+    exit 1
+fi
+
+# Set working directory and other variables
+HOMEDIR=/home/`whoami`
+REFERENCE=$1
+DATADIR=$2
+SAMPLE1=$3
+SAMPLE2=$4
+HOMEDIR=${HOMEDIR%%\/}
+DATADIR=${DATADIR%%\/}
 
 echo "Welcome to BLT!"
 
-a=$(basename $1)
-working_test=${a%%.*}
+mkdir $HOMEDIR/working
+mkdir $HOMEDIR/working/fastqc
 
-mkdir $working_test
-# cd $working_test
-mkdir $working_test/fast_qc
-
-cp $1 $working_test
-cp $2 $working_test
-cp $3 $working_test
-cp $4 $working_test
-cp $5 $working_test
-
-fastqc -o $working_test/fast_qc $2
-fastqc -o $working_test/fast_qc $3
-fastqc -o $working_test/fast_qc $4
-fastqc -o $working_test/fast_qc $5
-
-# for f in $(ls $DIR/$working_test/*.fna); do echo $f; bwa index $f; done
-
-bwa index $1
-
-s1=$(basename $2 .fastq)
-s2=$(basename $4 .fastq)
-
-bwa mem $1 $2 $3 > $working_test/$s1.sam
-bwa mem $1 $4 $5 > $working_test/$s2.sam
-
-samtools view -o $working_test/$s1.bam -b $working_test/$s1.sam
-samtools view -o $working_test/$s2.bam -b $working_test/$s2.sam
-
-samtools flagstat $working_test/$s1.bam > $working_test/$s1_align_stats.txt
-samtools flagstat $working_test/$s2.bam > $working_test/$s2_align_stats.txt
-
-samtools sort $working_test/$s1.bam -o $working_test/$s1_sorted.bam 
-samtools sort $working_test/$s2.bam -o $working_test/$s2_sorted.bam 
-
-samtools mpileup -f $1 $working_test/$s1_sorted.bam > $working_test/$s1.mpileup
-samtools mpileup -f $1 $working_test/$s2_sorted.bam > $working_test/$s2.mpileup
-
-java -Xmx4g -jar ~/VarScan.v2.4.3.jar mpileup2snp $working_test/$s1.mpileup --min-var-freq 0.7 --variants --output-vcf 1 > $working_test/$s1.vcf
-java -Xmx4g -jar ~/VarScan.v2.4.3.jar mpileup2snp $working_test/$s2.mpileup --min-var-freq 0.7 --variants --output-vcf 1 > $working_test/$s2.vcf
+echo "===========Starting FastQC==========="
+for f in $(ls $DATADIR/*.fastq); do echo "Performing fastqc on $f..."; fastqc -o $HOMEDIR/working/fastqc $f ; done
+echo "===========Done with FastQC==========="
 
 
+echo "===========Performing Alignment==========="
+# Copy all required files to the working directory
+cp $REFERENCE $HOMEDIR/working/
+cp $DATADIR/*.fastq $HOMEDIR/working/
+REFERENCE=${REFERENCE##*/}
+
+pushd $HOMEDIR/working/
+
+# Index the reference
+bwa index $REFERENCE
+
+echo "Align using bwa mem..."
+bwa mem $REFERENCE ${SAMPLE1}_1.fastq ${SAMPLE1}_2.fastq > ${SAMPLE1}.sam
+bwa mem $REFERENCE ${SAMPLE2}_1.fastq ${SAMPLE2}_2.fastq > ${SAMPLE2}.sam
+
+echo "Convert to bam..."
+samtools view -o ${SAMPLE1}.bam -b ${SAMPLE1}.sam
+samtools view -o ${SAMPLE2}.bam -b ${SAMPLE2}.sam
+
+echo "Get alignment stats..."
+samtools flagstat ${SAMPLE1}.bam > ${SAMPLE1}_align_stats.txt
+samtools flagstat ${SAMPLE2}.bam > ${SAMPLE2}_align_stats.txt
+
+echo "Sort the alignment..."
+samtools sort ${SAMPLE1}.bam -o ${SAMPLE1}_sorted.bam 
+samtools sort ${SAMPLE2}.bam -o ${SAMPLE2}_sorted.bam 
+
+popd
+
+echo "===========Done with Alignment==========="
+
+echo "===========Creating mpileup==========="
+pushd $HOMEDIR/working/
+
+samtools mpileup -f $REFERENCE ${SAMPLE1}_sorted.bam > ${SAMPLE1}.mpileup
+samtools mpileup -f $REFERENCE ${SAMPLE2}_sorted.bam > ${SAMPLE2}.mpileup
+
+popd
+echo "===========Done with mpileup==========="
+
+echo "===========Calling Variants==========="
+pushd $HOMEDIR/working/
+
+java -Xmx4g -jar ~/VarScan.v2.4.3.jar mpileup2snp ${SAMPLE1}.mpileup --min-var-freq 0.7 --variants --output-vcf 1 > ${SAMPLE1}.vcf
+java -Xmx4g -jar ~/VarScan.v2.4.3.jar mpileup2snp ${SAMPLE2}.mpileup --min-var-freq 0.7 --variants --output-vcf 1 > ${SAMPLE2}.vcf
+
+popd
+echo "===========Done with Variant Calling==========="
+
+
+
+echo "===========Analyzing SNPs==========="
 #VCF File comparison
-grep '#' $working_test/$s1.vcf > $working_test/VCF_header.txt
-header=$(grep -c '#' $working_test/$s1.vcf)
+#get header                                                                     
+
+pushd $HOMEDIR/working/
+mkdir tmp
+mkdir vcf_output
+
+# Assume VCF headers are similar, so save an arbitrary one
+grep '#' ${SAMPLE1}.vcf > tmp/VCF_header.txt
+header=$(grep -c '#' ${SAMPLE1}.vcf)
 header="$((header+1))"
 
-echo 'number of headers'
-echo $header
-tail -n +$header $working_test/$s1.vcf | awk '{printf("%s:%s:%s %s %s %s %s %s %s %s %s %s %s\n", $2, $4, $5, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10);}' | sort > $working_test/$s1_sorted_vcf
-tail -n +$header $working_test/$s2.vcf | awk '{printf("%s:%s:%s %s %s %s %s %s %s %s %s %s %s\n", $2, $4, $5, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10);}' | sort > $working_test/$s2_sorted_vcf
+tail -n +$header ${SAMPLE1}.vcf | awk '{printf("%s:%s:%s %s %s %s %s %s %s %s %s %s %s\n", $2, $4, $5, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10);}' | sort > ${SAMPLE1}_sorted_vcf
+tail -n +$header ${SAMPLE2}.vcf | awk '{printf("%s:%s:%s %s %s %s %s %s %s %s %s %s %s\n", $2, $4, $5, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10);}' | sort > ${SAMPLE2}_sorted_vcf
 
-mkdir $working_test/vcf_output
+join -1 1 -2 1 -o 1.3,1.5,1.6 ${SAMPLE1}_sorted_vcf ${SAMPLE2}_sorted_vcf > vcf_output/common.vcf
+join -1 1 -2 1 -v1 ${SAMPLE1}_sorted_vcf ${SAMPLE2}_sorted_vcf > vcf_output/only_${SAMPLE1}.vcf
+awk '{$(NF+1)=$1;$1=""}sub(FS,"")' vcf_output/only_${SAMPLE1}.vcf > tmp/only_${SAMPLE1}_orig.vcf
+cat tmp/VCF_header.txt tmp/only_${SAMPLE1}_orig.vcf > vcf_output/only_${SAMPLE1}.vcf
+join -1 1 -2 1 -v2 ${SAMPLE1}_sorted_vcf ${SAMPLE2}_sorted_vcf > vcf_output/only_${SAMPLE2}.vcf
+awk '{$(NF+1)=$1;$1=""}sub(FS,"")' vcf_output/only_${SAMPLE2}.vcf > tmp/only_${SAMPLE2}_orig.vcf
+cat tmp/VCF_header.txt tmp/only_${SAMPLE2}_orig.vcf > vcf_output/only_${SAMPLE2}.vcf
 
-join -1 1 -2 1 -o 1.3,1.5,1.6 $working_test/$s1_sorted_vcf $working_test/$s2_sorted_vcf > $working_test/vcf_output/common.vcf
+rm -rf tmp
 
-join -1 1 -2 1 -v1 $working_test/$s1_sorted_vcf $working_test/$s2_sorted_vcf > $working_test/vcf_output/only_$s1.vcf
-
-awk '{$(NF+1)=$1;$1=""}sub(FS,"")' $working_test/vcf_output/only_$s1.vcf > $working_test/vcf_output/only_$s1_orig.vcf
-
-cat $working_test/VCF_header.txt $working_test/vcf_output/only_$s1_orig.vcf > $working_test/vcf_output/only_$s1.vcf
-
-join -1 1 -2 1 -v2 $working_test/$s1_sorted_vcf $working_test/$s2_sorted_vcf > $working_test/vcf_output/only_$s2.vcf
-
-awk '{$(NF+1)=$1;$1=""}sub(FS,"")' $working_test/vcf_output/only_$s2.vcf > $working_test/vcf_output/only_$s2_orig.vcf
-
-cat $working_test/VCF_header.txt $working_test/vcf_output/only_$s2_orig.vcf > $working_test/vcf_output/only_$s2.vcf
+popd
+echo "===========Done analyzing SNPs==========="
