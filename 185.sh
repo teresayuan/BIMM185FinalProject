c3ced7974770f0d99e6eb469173ede68f12d1263
teresayuan <teryuan94@gmail.com>
2017-06-13 00:00:41 -0700

    switched contents of 185 and blt

--- 185.sh
+++ 185.sh
@@ -6,79 +6,86 @@
 # 4 --> forward strand of sample 2
 # 5 --> reverse strand of sample 2
 
-DIR=/home/bimm185
 
-# if [ "$1" == "-h" ] or ["$#" != 5]
-# then
-# 	if [ "$1" != "-h" ] 
-# 	then
-# 		echo "wrong number of files"
-# 	fi
-# 	echo "blt.sh reference sample1forward sample1reverse sample2forward sample2reverse"
+if [ "$1" == "-h" ] || [ "$#" -ne 5 ];
+then
+	if [ "$1" != "-h" ];
+	then
+		echo "wrong number of files"
+	fi
+	echo "BLT.sh reference sample1forward sample1reverse sample2forward sample2reverse"
 
-# 	exit 1
-# fi
+	exit 1
+fi
 
-echo "hello world"
+echo "Welcome to BLT!"
 
-mkdir $DIR/working_test
-mkdir $DIR/working_test/fastqc
+a=$(basename $1)
+working_test=${a%%.*}
 
-# for f in $(ls $DIR/data/*.fastq); do echo $f; fastqc -o $DIR/working_test/fastqc $f ; done
+mkdir $working_test
+# cd $working_test
+mkdir $working_test/fast_qc
 
-for f in $(ls $DIR/data/*.fastq); do echo $f; python $DIR/BIMM185FinalProject/modfastq.py $f ${f/data/working_test}; done
+cp $1 $working_test
+cp $2 $working_test
+cp $3 $working_test
+cp $4 $working_test
+cp $5 $working_test
 
+fastqc -o $working_test/fast_qc $2
+fastqc -o $working_test/fast_qc $3
+fastqc -o $working_test/fast_qc $4
+fastqc -o $working_test/fast_qc $5
 
+# for f in $(ls $DIR/$working_test/*.fna); do echo $f; bwa index $f; done
 
-#bwa index -p $f{${f/reference/working_test}%%.fna}; done
-cp $DIR/reference/GCF_000006945.2_ASM694v2_genomic.fna $DIR/working_test/.
-for f in $(ls $DIR/working_test/*.fna); do echo $f; bwa index $f; done
+bwa index $1
 
-# UC8
-bwa mem $DIR/working_test/GCF_000006945.2_ASM694v2_genomic.fna $DIR/working_test/UC8_1.fastq $DIR/working_test/UC8_2.fastq > $DIR/working_test/UC8.sam
-bwa mem $DIR/working_test/GCF_000006945.2_ASM694v2_genomic.fna $DIR/working_test/UC9_1.fastq $DIR/working_test/UC9_2.fastq > $DIR/working_test/UC9.sam
+s1=$(basename $2 .fastq)
+s2=$(basename $4 .fastq)
 
-samtools view -o $DIR/working_test/UC8.bam -b $DIR/working_test/UC8.sam
-samtools view -o $DIR/working_test/UC9.bam -b $DIR/working_test/UC9.sam
+bwa mem $1 $2 $3 > $working_test/$s1.sam
+bwa mem $1 $4 $5 > $working_test/$s2.sam
 
-samtools flagstat $DIR/working_test/UC8.bam > $DIR/working_test/UC8_align_stats.txt
-samtools flagstat $DIR/working_test/UC9.bam > $DIR/working_test/UC9_align_stats.txt
+samtools view -o $working_test/$s1.bam -b $working_test/$s1.sam
+samtools view -o $working_test/$s2.bam -b $working_test/$s2.sam
 
-samtools sort $DIR/working_test/UC8.bam -o $DIR/working_test/UC8_sorted.bam 
-samtools sort $DIR/working_test/UC9.bam -o $DIR/working_test/UC9_sorted.bam 
+samtools flagstat $working_test/$s1.bam > $working_test/$s1_align_stats.txt
+samtools flagstat $working_test/$s2.bam > $working_test/$s2_align_stats.txt
 
-samtools mpileup -f $DIR/working_test/GCF_000006945.2_ASM694v2_genomic.fna $DIR/working_test/UC8_sorted.bam > $DIR/working_test/UC8.mpileup
-samtools mpileup -f $DIR/working_test/GCF_000006945.2_ASM694v2_genomic.fna $DIR/working_test/UC9_sorted.bam > $DIR/working_test/UC9.mpileup
+samtools sort $working_test/$s1.bam -o $working_test/$s1_sorted.bam 
+samtools sort $working_test/$s2.bam -o $working_test/$s2_sorted.bam 
 
-java -Xmx4g -jar ~/VarScan.v2.4.3.jar mpileup2snp $DIR/working_test/UC8.mpileup --min-var-freq 0.7 --variants --output-vcf 1 > $DIR/working_test/UC8.vcf
-java -Xmx4g -jar ~/VarScan.v2.4.3.jar mpileup2snp $DIR/working_test/UC9.mpileup --min-var-freq 0.7 --variants --output-vcf 1 > $DIR/working_test/UC9.vcf
+samtools mpileup -f $1 $working_test/$s1_sorted.bam > $working_test/$s1.mpileup
+samtools mpileup -f $1 $working_test/$s2_sorted.bam > $working_test/$s2.mpileup
 
+java -Xmx4g -jar ~/VarScan.v2.4.3.jar mpileup2snp $working_test/$s1.mpileup --min-var-freq 0.7 --variants --output-vcf 1 > $working_test/$s1.vcf
+java -Xmx4g -jar ~/VarScan.v2.4.3.jar mpileup2snp $working_test/$s2.mpileup --min-var-freq 0.7 --variants --output-vcf 1 > $working_test/$s2.vcf
 
 
 #VCF File comparison
-#get header                                                                     
-
-grep '#' $DIR/working/UC8.vcf > $DIR/working_test/VCF_header.txt
-header=$(grep -c '#' $DIR/working/UC8.vcf)
+grep '#' $working_test/$s1.vcf > $working_test/VCF_header.txt
+header=$(grep -c '#' $working_test/$s1.vcf)
 header="$((header+1))"
 
 echo 'number of headers'
 echo $header
-tail -n +$header $DIR/working_test/UC8.vcf | awk '{printf("%s:%s:%s %s %s %s %s %s %s %s %s %s %s\n", $2, $4, $5, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10);}' | sort > $DIR/working_test/UC8_sorted_vcf
-tail -n +$header $DIR/working_test/UC9.vcf | awk '{printf("%s:%s:%s %s %s %s %s %s %s %s %s %s %s\n", $2, $4, $5, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10);}' | sort > $DIR/working_test/UC9_sorted_vcf
+tail -n +$header $working_test/$s1.vcf | awk '{printf("%s:%s:%s %s %s %s %s %s %s %s %s %s %s\n", $2, $4, $5, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10);}' | sort > $working_test/$s1_sorted_vcf
+tail -n +$header $working_test/$s2.vcf | awk '{printf("%s:%s:%s %s %s %s %s %s %s %s %s %s %s\n", $2, $4, $5, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10);}' | sort > $working_test/$s2_sorted_vcf
 
-mkdir $DIR/working_test/vcf_output
+mkdir $working_test/vcf_output
 
-join -1 1 -2 1 -o 1.3,1.5,1.6 $DIR/working_test/UC8_sorted_vcf $DIR/working_test/UC9_sorted_vcf > $DIR/working_test/vcf_output/common.vcf
+join -1 1 -2 1 -o 1.3,1.5,1.6 $working_test/$s1_sorted_vcf $working_test/$s2_sorted_vcf > $working_test/vcf_output/common.vcf
 
-join -1 1 -2 1 -v1 $DIR/working_test/UC8_sorted_vcf $DIR/working_test/UC9_sorted_vcf > $DIR/working_test/vcf_output/only_UC8.vcf
+join -1 1 -2 1 -v1 $working_test/$s1_sorted_vcf $working_test/$s2_sorted_vcf > $working_test/vcf_output/only_$s1.vcf
 
-awk '{$(NF+1)=$1;$1=""}sub(FS,"")' $DIR/working_test/vcf_output/only_UC8.vcf > $DIR/working_test/vcf_output/only_UC8_orig.vcf
+awk '{$(NF+1)=$1;$1=""}sub(FS,"")' $working_test/vcf_output/only_$s1.vcf > $working_test/vcf_output/only_$s1_orig.vcf
 
-cat $DIR/working_test/VCF_header.txt $DIR/working_test/vcf_output/only_UC8_orig.vcf > $DIR/working_test/vcf_output/only_UC8.vcf
+cat $working_test/VCF_header.txt $working_test/vcf_output/only_$s1_orig.vcf > $working_test/vcf_output/only_$s1.vcf
 
-join -1 1 -2 1 -v2 $DIR/working_test/UC8_sorted_vcf $DIR/working_test/UC9_sorted_vcf > $DIR/working_test/vcf_output/only_UC9.vcf
+join -1 1 -2 1 -v2 $working_test/$s1_sorted_vcf $working_test/$s2_sorted_vcf > $working_test/vcf_output/only_$s2.vcf
 
-awk '{$(NF+1)=$1;$1=""}sub(FS,"")' $DIR/working_test/vcf_output/only_UC9.vcf > $DIR/working_test/vcf_output/only_UC9_orig.vcf
+awk '{$(NF+1)=$1;$1=""}sub(FS,"")' $working_test/vcf_output/only_$s2.vcf > $working_test/vcf_output/only_$s2_orig.vcf
 
-cat $DIR/working_test/VCF_header.txt $DIR/working_test/vcf_output/only_UC9_orig.vcf > $DIR/working_test/vcf_output/only_UC9.vcf
+cat $working_test/VCF_header.txt $working_test/vcf_output/only_$s2_orig.vcf > $working_test/vcf_output/only_$s2.vcf
